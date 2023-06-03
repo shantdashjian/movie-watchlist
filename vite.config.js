1d2db830352241224c31de08b8cdc947913dbadd
shantdashjian <shantgdashjian@gmail.com>
2023-06-03 15:46:49 +0400

    Fix prod issue by having both html files copied to dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import {defineConfig} from 'vite'
-
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: []	
+	plugins: [],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				other: resolve(__dirname, 'watchlist.html')
+			}
+		}
+	}
 })
